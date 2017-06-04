af7fab52285ec19713810c3881a2ae58bff87bf6
James Reeves <jreeves@weavejester.com>
2017-06-04 22:39:17 +0100

    Initial commit

---
+++ test/duct/database/redis/carmine_test.clj
@@ -0,0 +1,7 @@
+(ns duct.database.redis.carmine-test
+  (:require [clojure.test :refer :all]
+            [duct.database.redis.carmine :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
