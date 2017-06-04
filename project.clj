2bd7c5da3aad61a7aab8bec34bc48f6875771949
James Reeves <jreeves@weavejester.com>
2017-06-04 23:41:16 +0100

    Add Carmine and Integrant dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [com.taoensso/carmine "2.16.0"]
+                 [integrant "0.4.0"]])
