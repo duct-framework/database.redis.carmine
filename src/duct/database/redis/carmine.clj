f88cbeecb9853034037c19533118f761dbaa428c
James Reeves <jreeves@weavejester.com>
2017-06-05 15:09:09 +0100

    Add Carmine key and boundary

--- src/duct/database/redis/carmine.clj
+++ src/duct/database/redis/carmine.clj
@@ -1,6 +1,11 @@
-(ns duct.database.redis.carmine)
+(ns duct.database.redis.carmine
+  (:require [taoensso.carmine :as car]
+            [integrant.core :as ig]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(derive :duct.database.redis/carmine :duct.database/redis)
+(derive :duct.database/redis         :duct/database)
+
+(defrecord Boundary [conn-opts])
+
+(defmethod ig/init-key :duct.database.redis/carmine [_ conn-opts]
+  (->Boundary conn-opts))
--- test/duct/database/redis/carmine_test.clj
+++ test/duct/database/redis/carmine_test.clj
@@ -1,7 +1,12 @@
 (ns duct.database.redis.carmine-test
   (:require [clojure.test :refer :all]
-            [duct.database.redis.carmine :refer :all]))
+            [duct.database.redis.carmine :refer :all]
+            [integrant.core :as ig]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest key-derive-test
+  (is (isa? :duct.database.redis/carmine :duct.database/redis))
+  (is (isa? :duct.database/redis :duct/database)))
+
+(deftest init-key-test
+  (is (= (ig/init-key :duct.database.redis/carmine {:spec {:host "127.0.0.1" :port 6379}})
+         (map->Boundary {:conn-opts {:spec {:host "127.0.0.1", :port 6379}}}))))
