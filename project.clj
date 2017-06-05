27a2e3559559b105978d3201bd1c8218d86e6dab
James Reeves <jreeves@weavejester.com>
2017-06-05 15:31:54 +0100

    Add URL and description to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject duct/database.redis.carmine "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Integrant methods for connecting to Redis via Carmine"
+  :url "https://github.com/duct-framework/database.redis.carmine"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
