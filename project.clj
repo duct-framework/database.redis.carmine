1a91ca7cdd8fa580356cd09f4a574f50200d94c9
James Reeves <jreeves@weavejester.com>
2018-06-16 18:53:08 +0100

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/database.redis.carmine "0.1.0"
+(defproject duct/database.redis.carmine "0.1.1"
   :description "Integrant methods for connecting to Redis via Carmine"
   :url "https://github.com/duct-framework/database.redis.carmine"
   :license {:name "Eclipse Public License"
